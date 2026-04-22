afa2145c3fabfddcd109c96c4f16eca8018bc6bf
LeeSF03 <shuenfeilee@gmail.com>
2026-04-22 17:11:49 +0800

    feat: setup uniwind with hero native ui

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -4,20 +4,27 @@ import {
   ThemeProvider,
 } from "@react-navigation/native";
 import React from "react";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { useColorScheme } from "react-native";
+import { HeroUINativeProviderRaw } from "heroui-native/provider-raw";
 
 import { AnimatedSplashOverlay } from "@/components/animated-icon";
 import AppTabs from "@/components/app-tabs";
 import { ConvexAuthProvider } from "@/providers/convex-better-auth";
+import "@/global.css";
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
   return (
-    <ConvexAuthProvider>
-      <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
-        <AnimatedSplashOverlay />
-        <AppTabs />
-      </ThemeProvider>
-    </ConvexAuthProvider>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <ConvexAuthProvider>
+        <HeroUINativeProviderRaw>
+          <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+            <AnimatedSplashOverlay />
+            <AppTabs />
+          </ThemeProvider>
+        </HeroUINativeProviderRaw>
+      </ConvexAuthProvider>
+    </GestureHandlerRootView>
   );
 }
